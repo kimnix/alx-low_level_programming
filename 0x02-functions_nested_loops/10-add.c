ed9745dd5c508dff5f9d8b7d4c019197abbdb249
kimnix <nixkimprop@gmail.com>
2023-08-15 05:25:23 -0700

    0x02. C - Functions, nested lubes

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+
+#include "main.h"
+
+/**
+ * add - sums two integers and returns the result
+ * int a: to be added to b
+ * int b: to be added to a
+ * Return: sum of a and b
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+
+	return (c);
+
