fb3a817ef9131c17dff70c07f8a24f55ac88ea6d
kimnix <nixkimprop@gmail.com>
2023-08-15 03:45:46 -0700

    1-alphabet.c3

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,18 @@
 #include "main.h"
+
 /**
- *main - print_alphabet in lowercase
+ *print_alphabet-print alphabets in lowercase
  */
+
+
 void print_alphabet(void)
 {
 	char letter;
-	for(letter='a';letter<='z';letter++)
+
+	for(letter = 'a'; letter <= 'z'; letter++)
 		_putchar(letter);
+
+
 	_putchar('\n');
-}
+} 
+
